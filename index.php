78c8bf19745d9c6fc8f882d3eb5ce30f18906073
gumonet <gutierrez.monola@gmail.com>
2022-06-14 16:29:37 -0500

    Remove key

--- index.php
+++ index.php
@@ -4,47 +4,41 @@ require 'vendor/autoload.php';
 
 use  \Aws\QuickSight\QuickSightClient;
 
-$client = new QuickSightClient([
-    'region' => 'us-east-1',
-    'version' => 'latest',
-    'credentials' => [
-        'key'    => "AKIAYHOLWTA7AN4OK6VE",
-        'secret' => "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"
-    ]
-]);
 
-$result = $client->generateEmbedUrlForAnonymousUser([
-        'AuthorizedResourceArns' => ['arn:aws:quicksight:us-east-1:565752272958:dashboard/3193f098-a090-4b47-b186-e7acc04a76b6'], // REQUIRED
-    'AwsAccountId' => '565752272958', // REQUIRED
-    'ExperienceConfiguration' => [ // REQUIRED
-        'Dashboard' => [
-            'InitialDashboardId' => '3193f098-a090-4b47-b186-e7acc04a76b6', // REQUIRED
+
+if( isset($_GET['if']) ) {
+    $client = new QuickSightClient([
+        'region' => 'us-east-1',
+        'version' => 'latest',
+        'credentials' => [
+           
+        ]
+    ]);
+
+    $report_id = 'null';
+    switch ( $_GET ){
+        case 'dinn':
+            $report_id = '565752272958:dashboard/3193f098-a090-4b47-b186-e7acc04a76b6';
+            break;
+        case 'castor':
+            $report_id = '565752272958:dashboard/3193f098-a090-4b47-b186-e7acc04a76b676';
+            break;
+        default:
+            break;
+    }
+
+    $result = $client->generateEmbedUrlForAnonymousUser([
+        'AuthorizedResourceArns' => ['arn:aws:quicksight:us-east-1:565752272958:dashboard/' . $report_id ], // REQUIRED
+        'AwsAccountId' => '565752272958', // REQUIRED
+        'ExperienceConfiguration' => [ // REQUIRED
+            'Dashboard' => [
+                'InitialDashboardId' => $report_id, // REQUIRED
+            ],
         ],
-    ],
-    'Namespace' => 'default', // REQUIRED
-    //'SessionLifetimeInMinutes' => 15,
-]);
-/**
- * $result = $client->generateEmbedUrlForAnonymousUser([
-    'AuthorizedResourceArns' => ['<string>', ...], // REQUIRED
-    'AwsAccountId' => '<string>', // REQUIRED
-    'ExperienceConfiguration' => [ // REQUIRED
-        'Dashboard' => [
-            'InitialDashboardId' => '<string>', // REQUIRED
-        ],
-    ],
-    'Namespace' => '<string>', // REQUIRED
-    'SessionLifetimeInMinutes' => <integer>,
-    'SessionTags' => [
-        [
-            'Key' => '<string>', // REQUIRED
-            'Value' => '<string>', // REQUIRED
-        ],
-        // ...
-    ],
-]);
- *
- */
+        'Namespace' => 'default', // REQUIRED
+        //'SessionLifetimeInMinutes' => 15,
+    ]);
+}
 
 ?>
 
@@ -70,20 +64,20 @@ $result = $client->generateEmbedUrlForAnonymousUser([
     <script type="text/javascript">
         var dashboard;
 
-        function embedDashboard() {
-            var containerDiv = document.getElementById("embeddingContainer");
-            var options = {
-                // replace this dummy url with the one generated via embedding API
-                //https://us-east-1.quicksight.aws.amazon.com/sn/accounts/565752272958/dashboards/519a1485-34e5-4cd5-b829-e620be6ff705?directory_alias=reworth
-                url: "<?php echo $result->get('EmbedUrl'); ?>",
-                container: containerDiv,
-                scrolling: "no",
-                height: "700px",
-                width: "100%",
-                footerPaddingEnabled: true
-            };
-            dashboard = QuickSightEmbedding.embedDashboard(options);
-        }
+        <?php if( $report_id !== 'null'){ ?>
+                function embedDashboard() {
+                    var containerDiv = document.getElementById("embeddingContainer");
+                    var options = {
+                        url: "<?php echo $result->get('EmbedUrl'); ?>",
+                        container: containerDiv,
+                        scrolling: "no",
+                        height: "700px",
+                        width: "100%",
+                        footerPaddingEnabled: true
+                    };
+                    dashboard = QuickSightEmbedding.embedDashboard(options);
+                }
+        <?php }?>
     </script>
 
     <body onload="embedDashboard()">
